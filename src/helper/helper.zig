c1c53cd0a0e2cc36db5335b6438855a90a9aa5dc
pratush <pratushrai0309@gmail.com>
2023-07-01 15:57:19 +0530

    Lexer: Added Strings

--- src/helper/helper.zig
+++ src/helper/helper.zig
@@ -1,19 +1,18 @@
 pub fn compare_string(str1: []const u8, str2: []const u8) bool {
-  var flag: i32 = 0;
-  if(str1.len != str2.len){
-    return false;
-  }
-  var i: usize = 0;
-  while (i < str1.len) {
-    if(str1[i]!=str2[i]){
-      flag = 1;
+    var flag: i32 = 0;
+    if (str1.len != str2.len) {
+        return false;
     }
-    i+=1;
-  }
-  if(flag == 0){
-    return true;
-  }
-  else {
-    return false;
-  }
+    var i: usize = 0;
+    while (i < str1.len) {
+        if (str1[i] != str2[i]) {
+            flag = 1;
+        }
+        i += 1;
+    }
+    if (flag == 0) {
+        return true;
+    } else {
+        return false;
+    }
 }
--- src/lexer/token.zig
+++ src/lexer/token.zig
@@ -1,76 +1,49 @@
 const std = @import("std");
 
-pub const TokenType = enum{
-  Illegal,
-  Eof,
-  Ident, 
-  Int, 
-  Assign,
-  Plus,
-  Comma,
-  SemiColon,
-  Gt,
-  Lt,
-  Minus,
-  Slash,
-  Star,
-  Lapren,
-  Rparen,
-  Lbrace,
-  Rbrace,
-  Bang,
-  Function,
-  Let,
-  False,
-  True,
-  Return,
-  If,
-  Else, 
-  EqEq,
-  BangEqual,
-  String
-};
+//TokenType Enum
+pub const TokenType = enum { Illegal, Eof, Ident, Int, Assign, Plus, Comma, SemiColon, Gt, Lt, Minus, Slash, Star, Lapren, Rparen, Lbrace, Rbrace, Bang, Function, Let, False, True, Return, If, Else, EqEq, BangEqual, String };
 
-pub fn ttype_to_string(ttype: TokenType) []const u8{
-  var s = switch (ttype) {
-    TokenType.Illegal => return "Illegal",
-    TokenType.Eof => return "Eof",
-    TokenType.Ident => return "Ident",
-    TokenType.Int => return "Int",
-    TokenType.Assign => return "Assign",
-    TokenType.Plus => return "Plus",
-    TokenType.Comma => return "Comma",
-    TokenType.SemiColon => return "SemiColon",
-    TokenType.Lapren => return "Lparen",
-    TokenType.Rparen => return "Rparen",
-    TokenType.Lbrace => return "Lbrace",
-    TokenType.Rbrace => return "Rbrace",
-    TokenType.Gt => return "Gt",
-    TokenType.Lt => return "Lt",
-    TokenType.Minus => return "Minus",
-    TokenType.Slash => return "Slash",
-    TokenType.Star => return "Star",
-    TokenType.Function => return "Function",
-    TokenType.Let => return "Let",
-    TokenType.False => return "False",
-    TokenType.True => return "True",
-    TokenType.Return => return "Return",
-    TokenType.If => return "If",
-    TokenType.Else => return "Else",
-    TokenType.EqEq => return "EqEq",
-    TokenType.Bang => return "Bang",
-    TokenType.BangEqual => return "BangEqual",
-    TokenType.String => return "String"
-  };
-  return s;
+//Converts TokenType to String Literal Ease for displaying
+pub fn ttype_to_string(ttype: TokenType) []const u8 {
+    var s = switch (ttype) {
+        TokenType.Illegal => return "Illegal",
+        TokenType.Eof => return "Eof",
+        TokenType.Ident => return "Ident",
+        TokenType.Int => return "Int",
+        TokenType.Assign => return "Assign",
+        TokenType.Plus => return "Plus",
+        TokenType.Comma => return "Comma",
+        TokenType.SemiColon => return "SemiColon",
+        TokenType.Lapren => return "Lparen",
+        TokenType.Rparen => return "Rparen",
+        TokenType.Lbrace => return "Lbrace",
+        TokenType.Rbrace => return "Rbrace",
+        TokenType.Gt => return "Gt",
+        TokenType.Lt => return "Lt",
+        TokenType.Minus => return "Minus",
+        TokenType.Slash => return "Slash",
+        TokenType.Star => return "Star",
+        TokenType.Function => return "Function",
+        TokenType.Let => return "Let",
+        TokenType.False => return "False",
+        TokenType.True => return "True",
+        TokenType.Return => return "Return",
+        TokenType.If => return "If",
+        TokenType.Else => return "Else",
+        TokenType.EqEq => return "EqEq",
+        TokenType.Bang => return "Bang",
+        TokenType.BangEqual => return "BangEqual",
+        TokenType.String => return "String",
+    };
+    return s;
 }
 
+//Token Struct
 pub const Token = struct {
-  ttype: TokenType,
-  literal:  []const u8,
-  
-  pub fn print_token(t: Token) void {
-    std.debug.print("[ ttype: {s}, literal: {s} ]\n", .{ttype_to_string(t.ttype), t.literal});
-  }
-};
+    ttype: TokenType,
+    literal: []const u8,
 
+    pub fn print_token(t: Token) void {
+        std.debug.print("[ ttype: {s}, literal: {s} ]\n", .{ ttype_to_string(t.ttype), t.literal });
+    }
+};
